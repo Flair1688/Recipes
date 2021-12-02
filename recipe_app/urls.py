64aa17815a9ab60d033ca0f76d6e20103b8d0a88
Islam <islam@gmail.com>
2021-12-03 00:58:42 +0600

    post_detail created

--- recipe_app/urls.py
+++ recipe_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import RecipeList
+from .views import RecipeList, RecipeDetailView
 
 urlpatterns = [
-    path('', RecipeList.as_view(), name='home'),
+    path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('', RecipeList.as_view(), name='recipe_list'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
-
+from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.shortcuts import render, get_object_or_404
 
 
 class RecipeList(ListView):
     model = Recipe
     template_name = 'recipe_list.html'
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe_app/recipe_detail.html'
+
+    #def recipe_detail(request, pk):
+     #   recipe = get_object_or_404(Recipe, pk=pk)
+      #  return render(request, 'recipe_app/recipe_detail.html', {'recipe': recipe})
